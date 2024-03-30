03a632932f620d5f370b0563f9b8f4ffaf206cfa
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-03-30 18:20:35 +0800

    Add static and job data

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Job, GalleryImage
+
+class GalleryImageInline(admin.TabularInline):
+    model = GalleryImage
+    extra = 3  # Number of extra fields to show
 
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'salary', 'company_name', 'date_created')
+    list_filter = ('company_name', 'date_created')
+    search_fields = ('title', 'company_name')
+    readonly_fields = ('date_created',)
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(GalleryImage)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=150)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    company_email = models.EmailField()
+    company_contacts = models.CharField(max_length=100)
+    company_map = models.CharField(max_length=280)
+    profile_image = models.ImageField(upload_to='static/images/job_profile_images/')
+    date_created = models.DateTimeField(auto_now_add=True)
+    company_name = models.CharField(max_length=150)
+    company_logo = models.ImageField(upload_to='static/images/company_logos/')
+
+    def __str__(self):
+        return self.title
+
+class GalleryImage(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='static/images/job_gallery/')
+
+    def __str__(self):
+        return f"Image for {self.job.title}"
