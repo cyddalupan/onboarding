1efaf4628700147b391916a90c510bbe1b0483b9
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-03-30 18:45:00 +0800

    Add customer app

---
+++ customer/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Customer(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    mobile = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+class Attachment(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='static/customer_attachments/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Attachment for {self.customer.firstname} {self.customer.lastname}"
