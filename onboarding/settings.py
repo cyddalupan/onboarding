11088cc458bdad39d1a65b9f757cc0302c03a197
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-04-07 14:53:20 +0800

    add cors to api

--- onboarding/settings.py
+++ onboarding/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'jobs',
     'customer',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'onboarding.urls'
