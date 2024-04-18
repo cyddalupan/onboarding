4269d091eef16696c6af7e0897b3d4a9bc85a443
cyd dalupan <cydmdalupan@gmail.com>
2024-04-18 05:15:17 +0000

    customer crud

--- customer/urls.py
+++ customer/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-#from .views import HelloWorld
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import CustomerCreateView, CustomerRetrieveUpdateDeleteView
 
 urlpatterns = [
-    
+    path('', CustomerCreateView.as_view(), name='customer-list-create'),
+    path('<int:pk>/', CustomerRetrieveUpdateDeleteView.as_view(), name='customer-detail'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import CustomerSerializer
+from .models import Customer
+
+class CustomerCreateView(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CustomerRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
