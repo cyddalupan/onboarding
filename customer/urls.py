ecf0fae472d2fcbee0241c2f3db8ec66cf45c9a3
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-04-21 17:30:48 +0800

    Add login feature

--- customer/urls.py
+++ customer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import CustomerCreateView, CustomerRetrieveUpdateDeleteView
+from .views import CustomerCreateView, CustomerLoginView, CustomerRetrieveUpdateDeleteView
 
 urlpatterns = [
     path('', CustomerCreateView.as_view(), name='customer-list-create'),
     path('<int:pk>/', CustomerRetrieveUpdateDeleteView.as_view(), name='customer-detail'),
+    path('login/', CustomerLoginView.as_view(), name='customer-login'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .serializers import CustomerSerializer
 from .models import Customer
@@ -11,3 +14,19 @@ class CustomerCreateView(generics.ListCreateAPIView):
 class CustomerRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+class CustomerLoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        try:
+            customer = get_object_or_404(Customer, username=username)
+        except Customer.DoesNotExist:
+            return Response({'error': 'Username incorrect'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if customer.password != password:
+            return Response({'error': 'Password incorrect'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data, status=status.HTTP_200_OK)
