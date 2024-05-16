70c4b863770fcb8b3e29839ca5f3d6880a9f46b4
Cleddrence Dalupan <cydmdalupan@gmail.com>
2024-05-16 20:19:11 +0800

    Add config save

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('jobs/', include('jobs.urls')),
     path('customer/', include('customer.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
